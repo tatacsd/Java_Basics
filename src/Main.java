fb48f23ff475762dc68cbfa7d9af5d099fa03714
Thays <thayscasado@hotmail.com>
2020-12-21 01:43:15 -0800

    Challenge Multiple Choice

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
@@ -46,7 +47,7 @@ public class Main {
             System.out.println("Your shoe selection will make you happy today");
         }
          */
-        
+        /*
         Scanner input = new Scanner(System.in);
         boolean isOnRepeat = true;
 
@@ -61,6 +62,45 @@ public class Main {
         }
 
         System.out.println("Playing next song");
+         */
+
+
+        // ***************** CHALLENGE MULTIPLE CHOICE ****************
+
+        String question = "What is the biggest desert on earth ?";
+        String choiceOne = "antarctic";
+        String choiceTwo = "sahara";
+        String choiceThree = "artic";
+        String choiceFour = "arabian";
+
+        String correctAnswer = choiceOne;
+
+        // Write a print statement asking the question
+        System.out.println(question);
+        // Write a print statement giving the answer choices
+        System.out.println("Choose one of the following: " + choiceOne +
+                ", " + choiceTwo + ", " + choiceThree + " or " + choiceFour);
+
+        // Have the user input answer
+        Scanner Input = new Scanner(System.in);
+        // Retrieve the user's input
+        String userInput = Input.next();
+
+        // If the user's input matches the correctAnswer..
+        if(correctAnswer.equals(userInput.toLowerCase())){
+            // then the user is correct and we want to print out a congrats message to the user
+            System.out.println("***********************************************");
+            System.out.println("Congratulations, you got the right answer");
+
+        } else{
+            // If the user does not match the correctAnswer...
+            // then the user is incorrect and we want to print out a message saying that the user is wrong
+            System.out.println("***********************************************");
+            System.out.println("I'm sorry, " + userInput + " is not the right answer. The correct answer is " + correctAnswer);
+        }
+
+
+
 
 
     }
