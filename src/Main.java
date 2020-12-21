05f733046f6e5b384ca693c88424943a5d15cfc4
Thays <thayscasado@hotmail.com>
2020-12-21 00:09:12 -0800

    2.4 Using indexes with string >> charAt

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,14 @@ public class Main {
         //System.out.println("Hello World from the Intelij IDE");
         int studentAge = 28;
         double studentGPA = 3.45;
-        char studentFirstInitial = 'T';
-        char studentLastInitial = 'C';
-        boolean hasPerfectAttendence = true;
         String studentFirstName = "Thays";
         String studentLastName = "Casado";
+        //char studentFirstInitial = 'T';
+        char studentFirstInitial = studentFirstName.charAt(0);
+        //char studentLastInitial = 'C';
+        char studentLastInitial = studentLastName.charAt(0);
+        boolean hasPerfectAttendence = true;
+
 
         System.out.println(studentAge);
         System.out.println(studentGPA);
