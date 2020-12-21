450a62526a43d69b135173a02564270a9c344f43
Thays <thayscasado@hotmail.com>
2020-12-20 23:54:20 -0800

    data types and variables class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World from the Intelij IDE");
+        //System.out.println("Hello World from the Intelij IDE");
+        int studentAge = 28;
+        double studentGPA = 3.45;
+        char studentFirstInitial = 'T';
+        char studentLastInitial = 'C';
+        boolean hasPerfectAttendence = true;
     }
 }
