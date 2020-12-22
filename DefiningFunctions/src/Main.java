c2d2dde416dae00117b12a73d2abddc88da605a2
Thays <thayscasado@hotmail.com>
2020-12-21 23:57:22 -0800

    calling a function

--- DefiningFunctions/src/Main.java
+++ DefiningFunctions/src/Main.java
@@ -6,9 +6,18 @@ public class Main {
         System.out.println("Type in a random word and press Enter to start developer tea time");
         Scanner input = new Scanner(System.in);
         input.next();
-        System.out.println("It's developer te time.");
+        System.out.println("It's developer tea time.");
     }
     public static void main(String[] args){
+        System.out.println("Welcome to your new job");
 
+        announceDeveloperTeaTime();
+
+        System.out.println("Write code");
+        System.out.println("Review Code");
+
+        announceDeveloperTeaTime();
+
+        System.out.println("Get promoted");
     }
 }
