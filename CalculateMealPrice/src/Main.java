bf5cc3b53d260f0198ce06515cc563cace3d8843
Thays <thayscasado@hotmail.com>
2020-12-22 00:27:16 -0800

    return type function

--- CalculateMealPrice/src/Main.java
+++ CalculateMealPrice/src/Main.java
@@ -1,28 +1,20 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void CalculateMealPrice(double listedMealPrice, double tipRate, double taxRate){
-        double tip = (tipRate/100) * listedMealPrice;
-        double tax = (taxRate/100) * listedMealPrice;
+    public static double CalculateMealPrice(double listedMealPrice, double tipRate, double taxRate){
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
         double result = tip + tax + listedMealPrice;
 
-        System.out.println("Your total meal price is: " + result);
+        return result;
     }
     public static void main(String[] args){
-        /*
-        System.out.println("Type your meal price: ");
-        Scanner input = new Scanner(System.in);
-        double listedMealPrice = input.nextDouble();
-        System.out.println("Type the amount of tips in %: ");
-        double tipRate = input.nextDouble();
-        System.out.println("Type the tax rate in %: ");
-        double taxRate = input.nextDouble();
 
-        CalculateMealPrice(listedMealPrice, tipRate, taxRate);
-         */
-        CalculateMealPrice(15, 10, 10);
+        double groupMealPrice = CalculateMealPrice(15, 0.1, 0.12);
+        System.out.println(groupMealPrice);
 
-
+        double individualMealPrice = groupMealPrice / 5;
+        System.out.println(individualMealPrice);
 
     }
 }
