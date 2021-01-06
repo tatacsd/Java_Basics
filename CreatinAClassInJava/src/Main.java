2df6f750b453a601fbd39bb5d8106816426a608c
Thays <thayscasado@hotmail.com>
2021-01-06 11:30:28 -0800

    6. Classes in Java >>  Using instance methods in java

--- CreatinAClassInJava/src/Main.java
+++ CreatinAClassInJava/src/Main.java
@@ -6,8 +6,17 @@ public class Main {
 		Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
 		Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
 
+		// Use the instance method (created in Triangle Class) to find the area
+		// Of this triangle.
 		double triangleArea = triangleA.findArea();
 		System.out.println(triangleArea);
+		
+		double triangleBArea = triangleB.findArea();
+		System.out.println(triangleBArea);
+		
+		// Triangle.findArea() --> Math.pow
+		// instance method vs. class method
+		
 	}
 
 }
