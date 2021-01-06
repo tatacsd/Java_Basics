712b38c7b6cf5ad411fc7f095baec08c172498dd
Thays <thayscasado@hotmail.com>
2021-01-06 11:26:02 -0800

    6. Classes in Java >> Instance methods vs. class methods

--- CreatinAClassInJava/src/Main.java
+++ CreatinAClassInJava/src/Main.java
@@ -6,6 +6,8 @@ public class Main {
 		Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
 		Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
 
+		double triangleArea = triangleA.findArea();
+		System.out.println(triangleArea);
 	}
 
 }
