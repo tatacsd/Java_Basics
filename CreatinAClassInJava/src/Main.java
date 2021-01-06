5b53b87aeda93cbe319a4b23f8f5184bfe127ef8
Thays <thayscasado@hotmail.com>
2021-01-06 11:18:33 -0800

    6. Classes In Java >> Creating instances in Java

--- CreatinAClassInJava/src/Main.java
+++ CreatinAClassInJava/src/Main.java
@@ -2,7 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Creating Intances
+		Triangle triangleA = new Triangle(15, 8, 15, 8, 17);
+		Triangle triangleB = new Triangle(3, 2.598, 3, 3, 3);
 
 	}
 
